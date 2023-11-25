3c3e66a3bf3b955c8b2e6facce7f06281f32175f
1e1104k4 <1e1104k4@gmail.com>
2023-11-24 22:33:19 -0600

    Initial Commit

---
+++ cerebrumSquall/cerebrumSquallApp.swift
@@ -0,0 +1,17 @@
+//
+//  cerebrumSquallApp.swift
+//  cerebrumSquall
+//
+//  Created by Leila on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct cerebrumSquallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
