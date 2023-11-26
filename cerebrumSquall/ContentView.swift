31ba61946f750cae38597a9b20f0a0c3bcd1cc71
1e1104k4 <1e1104k4@gmail.com>
2023-11-25 18:41:30 -0600

    user move selection buttons functionality

--- cerebrumSquall/ContentView.swift
+++ cerebrumSquall/ContentView.swift
@@ -9,27 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var movesChoice = ["rock", "paper", "scissors"]
-    @State var currentMoveChoice = Int.random(in: 0...2)
+    let movesChoice = ["rock", "paper", "scissors"]
+    @State var computerMoveChoice = Int.random(in: 0..<3)
     @State var shouldWin = Bool.random()
-    
+
     var body: some View {
         VStack {
             HStack {
+                
                 VStack {
                     Text("game chose:")
                     Text("you should:")
                 }
                 VStack {
-                    Text(movesChoice[currentMoveChoice])
+                    Text(movesChoice[computerMoveChoice])
                     Text(shouldWin ? "win" : "lose")
+                }
+            }
+            HStack {
+                VStack {
+                    Text("your chose:")
+                }
+                VStack {
+                    ForEach(0..<3) { moveChoice in
+                        Button(movesChoice[moveChoice]) {
+                            userMove(is: moveChoice)
+                            
+                        }
+                        
+                    }
                 }
             }
             
         }
         .padding()
+        
+        
+    } // closing brase for body
+    
+    func userMove(is moveChoice: Int) {
+        let winningMovesChoice = [1, 2, 0]
+        let didWin: Bool
+        
+        if shouldWin {
+            didWin = moveChoice == winningMovesChoice[computerMoveChoice]
+        } else {
+            didWin = winningMovesChoice[moveChoice] == computerMoveChoice
+        }
+        
     }
-}
+    
+} // closing brase for struct
 
 #Preview {
     ContentView()
